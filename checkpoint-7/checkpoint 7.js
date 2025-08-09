2248c62563da1b1de1ed80b3848d1e9981145ec8
arenedoparra <ane.r98@gmail.com>
2025-08-09 11:27:18 +0200

    Ej JS_checkpoint 7

--- checkpoint-7/checkpoint 7.js
+++ checkpoint-7/checkpoint 7.js
@@ -3,3 +3,14 @@
 // Si el número creado es mayor que 50, la consola registra "¡El número es mayor que 50!". 
 // Si es más pequeño, la consola registra "¡El número es menor que 50!"
 
+function argumentos(a, b, c, d) {
+  const suma1 = a + b; 
+  const suma2 = c + d;
+  const resultado = suma1 * suma2;
+
+  if (resultado > 50) {
+    console.log("¡El número es mayor que 50!");
+  } else {
+    console.log("¡El número es menor que 50!");
+  }
+}

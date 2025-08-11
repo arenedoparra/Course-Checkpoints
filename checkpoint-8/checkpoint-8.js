5ef621f22b53a27b041112214f94426adb3fd550
arenedoparra <ane.r98@gmail.com>
2025-08-11 17:31:28 +0200

    Checkpoint 8 practico JS

---
+++ checkpoint-8/checkpoint-8.js
@@ -0,0 +1,24 @@
+// Cree un bucle for en JS que imprima cada nombre en esta lista.
+// miLista = “velma”, “exploradora”, “jane”, “john”, “harry”
+
+let miLista = ["velma", "exploradora", "jane", "john", "harry"];
+
+for (let i = 0; i < miLista.length; i++) {
+    console.log(miLista[i]);
+}
+
+// Cree un bucle while que recorra la misma lista y también imprima los nombres. 
+// Nota: Recuerda crear un contador para que el ciclo no sea infinito.
+
+let miLista = ["velma", "exploradora", "jane", "john", "harry"];
+
+let contador = 0;
+while (contador < miLista.length) {
+    console.log(miLista[contador]);
+    contador++;
+}
+
+// Cree una función de flecha que devuelva "Hola mundo".
+
+const saludar = () => "Hola mundo";
+console.log(saludar());
